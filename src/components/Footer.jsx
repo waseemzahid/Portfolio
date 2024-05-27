a3291049987f30c3fdc81c1d9a9cedbdc8a9930b
waseemzahid <34806507+waseemzahid@users.noreply.github.com>
2024-05-27 21:09:33 +0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,18 +3,18 @@ import logo from '../assets/favicon.png'
 
 const Footer = () => {
   return (
-    <div className='mx-auto max-w-[1200px] sm:h-[150px] p-12 flex items-center justify-between'>
-      <a href='/'>
-        <div className='flex items-center'>
-        <img src={logo} alt='' width={40} height={30} />
-        <h1 className='text-2xl font-bold primary-color ml-2'>Waseem Zahid</h1>
-      </div>
-      </a>
-      <p className='text-white sm:text-lg lg:text-xl'>
+      <div className='mx-auto max-w-[1200px] p-12 flex items-center justify-between flex-col sm:flex-row sm:h-[150px]'>
+        <a href='/'>
+          <div className='flex items-center'>
+            <img src={logo} alt='' width={40} height={30} />
+            <h1 className='text-2xl font-bold primary-color ml-2'>Waseem Zahid</h1>
+          </div>
+        </a>
+        <p className='text-white text-center sm:text-left sm:text-lg lg:text-xl mt-4 sm:mt-0'>
           <a href='mailto:waseemzahid64@gmail.com'>waseemzahid64@gmail.com</a> <br /> 
-          <a href="tel:+923006997639">Conatct: +92 300 6997639</a>
+          <a href="tel:+923006997639">Contact: +92 300 6997639</a>
         </p>
-    </div>
+      </div>
   )
 }
 
